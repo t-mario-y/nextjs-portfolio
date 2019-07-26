0d69a9dda4b1e84b3c88262779455e834b5a2945
t-mario-y-windows-pc-2 <t.mario.yoshi@gmail.com>
2019-07-26 12:07:40 +0900

    material-uiを導入

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,14 @@
 import Layout from '../components/MyLayout.js';
+import Button from '@material-ui/core/Button';
 
 const About = () => (
   <div>
     <Layout>
       <p>This is the about page.</p>
     </Layout>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
   </div>
 );
 
